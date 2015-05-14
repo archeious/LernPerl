fb38f4d90c912b074eab0185d8d76b714252e801
Jeff 'Archeious' Smith <me@archeio.us>
2015-05-14 16:22:59 -0600

    Added first task mark II, this time with maps.  Well a map.  There is only one map

---
+++ first_task/first_task_mkII.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl -w
+
+=head1 NAME
+first_task - prints on individual lines number range from 5 to 500
+
+=head1 AVAILABILITY 
+https://github.com/archeious/LernPerl/
+
+=head1 AUTHOR
+Jeff Smith - L<https://archeio.us/>
+=cut
+
+use strict;
+use warnings;
+
+
+my $firstNumber = 5;
+my $lastNumber = 500;
+
+my @arr = ($firstNumber..$lastNumber);
+map { printf "%d\n", $_ } @arr;
