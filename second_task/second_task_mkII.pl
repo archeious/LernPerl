a0568d65fced5fcdeb7002f7c09cf1f9abc64f82
Jeff 'Archeious' Smith <me@archeio.us>
2015-05-21 14:52:30 -0600

    Adding second_task Mark II which uses the for post operation syntax

---
+++ second_task/second_task_mkII.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl -w
+=head1 NAME
+second_task - prints on individual lines number range from 5 to 500 using an array 
+without using a variable?
+
+=head1 AVAILABILITY
+https://github.com/archeious/LernPerl/
+
+=head1 AUTHOR
+Jeff Smith - L<https://archeio.us/>
+=cut
+
+use strict;
+use warnings;
+
+print "$_\n" for (5..500);
