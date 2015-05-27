6fb2b391b2c09dc5e110dca1fb27d472bb309ccd
Jeff 'Archeious' Smith <me@archeio.us>
2015-05-27 15:32:36 -0600

    Added Print Odd numbers "Extra Credit" now with unlessgit add second_task_mkIV.pl !

---
+++ second_task/second_task_mkIV.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl -w
+=head1 NAME
+second_task - prints on individual lines number range from 5 to 500 using an array 
+without using a variable?
+
+=head1 AVAILABILITY
+https://github.com/archeious/LernPerl/
+
+=head1 AUTHOR
+Jeff Smith - L<https://archeio.us/>
+=cut
+
+use strict;
+use warnings;
+
+foreach (5..500){
+    print "$_\n" unless !($_%2)
+}
