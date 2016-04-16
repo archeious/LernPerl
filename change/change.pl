729cdd6013b83a7d7a1c958fd7318799bbd8cdd8
Jeff Smith <jeff.smith@endurance.com>
2016-04-15 18:51:11 -0600

    Alt currency is now a hash.  Error checking to make sure currency data is valid.  Code is generally cleaner.

--- change/change.pl
+++ change/change.pl
@@ -2,36 +2,61 @@
 
 use strict;
 use warnings;
+use Scalar::Util qw(looks_like_number);
 use Data::Dumper;
 
 sub change {
-    print  "args:". Data::Dumper->Dumper(@_)."\n";
-    my $amount = shift;
-    my @currency  =  shift || (
-        {name => 'Five Hundred Dollar Bill', value => 500},  
-        {name => 'One Hundred Dollar Bill',  value => 100},  
-        {name => 'Fifty Dollar Bill',        value => 50},  
-        {name => 'Twenty Dollar Bill',       value => 20},  
-        {name => 'Ten Dollar Bill',          value => 10},  
-        {name => 'Five Dollar Bill',         value => 5},  
-        {name => 'Dollar Bill',              value => 1},  
-        {name => 'Quarter',                  value => 0.25},  
-        {name => 'Dime',                     value => 0.10},  
-        {name => 'Nickel',                   value => 0.05},  
-        {name => 'Penny',                    value => 0.01},  
-    );
-    print  "currency". Data::Dumper->Dumper(@currency)."\n";
-    my $change = {};    
+    my $amount = shift || die "Missing parameter: Cannot give change for non-existent values";
+    my $currency = shift || {  # USD is the default currency if none is provided
+        base => 100,
+        units => [
+            {name => 'Five Hundred Dollar Bill', value => 50000},  
+            {name => 'One Hundred Dollar Bill',  value => 10000},  
+            {name => 'Fifty Dollar Bill',        value => 5000},  
+            {name => 'Twenty Dollar Bill',       value => 2000},  
+            {name => 'Ten Dollar Bill',          value => 1000},  
+            {name => 'Five Dollar Bill',         value => 500},  
+            {name => 'Dollar Bill',              value => 100},  
+            {name => 'Quarter',                  value => 25},  
+            {name => 'Dime',                     value => 10},  
+            {name => 'Nickel',                   value => 5},  
+            {name => 'Penny',                    value => 1},  
+        ]
+    };
+    
+    die "Currency is missing base value" unless exists $currency->{base};  
+    $amount = $amount * $currency->{base}; 
+    my $change = { };   
+
+    die "Currency units are missing" unless exists $currency->{units};  
+    die "Must declare are least one currency units" unless @{$currency->{units}} > 0;  
     do {
-        my $unit = shift @currency;
-        print  "unit:". Data::Dumper->Dumper($unit)."\n";
+        my $unit = shift $currency->{units};
+        die "Missing currency unit name" unless exists $unit->{name};  
+        die "Currency unit $unit->{name} is missing a value" unless exists $unit->{value};  
+        die "Currency unit $unit->{name} is not a number" unless looks_like_number($unit->{value});  
+        die "Currency unit $unit->{name} is not positive" unless $unit->{value} > 0; 
         if ($amount >= $unit->{value}) {
             my $numOfUnits = int($amount/$unit->{value});
             $change->{$unit->{name}} = $numOfUnits;
             $amount -= $numOfUnits * $unit->{value};
-        }        
-    } while ($amount > 0);
+        }
+    # if the remaining amount or currency units run out bail out of the loop        
+    } while ($amount > 0 && @{$currency->{units}} > 0);  
     return $change;
 }
 
-print  Data::Dumper->Dumper(change(1234.0,({name=>'Dimes', value=>'0.10'},{name=>'Nickels',value=>'0.05'})))."\n";
+print "Change for 1234.56 with US currency\n" .Data::Dumper->Dumper(change(1234.56))."\n";
+
+my $fiveAndDime = { 
+    base => 100,
+    units => [
+        {name => 'Dime',                     value => "10"},  
+        {name => 'Nickel',                   value => 5},  
+    ]
+};
+
+print "Change for 1234.56 at a Five and Dime store (aka only nickels and dimes)\n". 
+    Data::Dumper->Dumper(change(1234.56,$fiveAndDime))."\n";
+    
+    
