c91b8601c66905e83a289f1e21f06e5160dbcc14
archeious <github@unbiasedgeek.com>
2016-04-15 19:37:14 -0600

    Last clean up for the night I promise. :)

--- change/change.pl
+++ change/change.pl
@@ -3,64 +3,63 @@
 use strict;
 use warnings;
 use Scalar::Util qw(looks_like_number);
+use Carp qw( croak );
 use Data::Dumper;
 
 sub change {
-    my $amount = shift || die "Missing parameter: Cannot give change for non-existent values";
+    my $amount = shift || croak 'Missing parameter: Cannot give change for non-existent values';
     my $currency = shift || {  # USD is the default currency if none is provided
         base => 100,
         units => [
-            {name => 'Five Hundred Dollar Bill', value => 50000},  
-            {name => 'One Hundred Dollar Bill',  value => 10000},  
-            {name => 'Fifty Dollar Bill',        value => 5000},  
-            {name => 'Twenty Dollar Bill',       value => 2000},  
-            {name => 'Ten Dollar Bill',          value => 1000},  
-            {name => 'Five Dollar Bill',         value => 500},  
-            {name => 'Dollar Bill',              value => 100},  
-            {name => 'Quarter',                  value => 25},  
-            {name => 'Dime',                     value => 10},  
-            {name => 'Nickel',                   value => 5},  
-            {name => 'Penny',                    value => 1},  
+            {name => 'Five Hundred Dollar Bill', value => 50_000},
+            {name => 'One Hundred Dollar Bill',  value => 10_000},
+            {name => 'Fifty Dollar Bill',        value => 5_000},
+            {name => 'Twenty Dollar Bill',       value => 2_000},
+            {name => 'Ten Dollar Bill',          value => 1_000},
+            {name => 'Five Dollar Bill',         value => 500},
+            {name => 'Dollar Bill',              value => 100},
+            {name => 'Quarter',                  value => 25},
+            {name => 'Dime',                     value => 10},
+            {name => 'Nickel',                   value => 5},
+            {name => 'Penny',                    value => 1},
         ]
     };
-        
-    die "Amount provided is not a number" unless looks_like_number($amount);          
-    die "Currency is missing base value" unless exists $currency->{base};  
-    die "Currency base value is not a number" unless looks_like_number($currency->{base});      
-    $amount = $amount * $currency->{base};
-     
-    my $change = { };   
 
-    die "Must declare are least one currency units" unless exists $currency->{units} && @{$currency->{units}} > 0;  
+    croak 'Amount provided is not a number' unless looks_like_number($amount);
+    croak 'Currency is missing base value' unless exists $currency->{base};
+    croak 'Currency base value is not a number' unless looks_like_number($currency->{base});
+    $amount = $amount * $currency->{base};
+
+    my $change = { };
+
+    croak 'Must declare are least one currency units' unless exists $currency->{units} && @{$currency->{units}} > 0;
     do {
         # Grab next currency unit
         my $unit = shift $currency->{units};
-
-        die "Missing currency unit name" unless exists $unit->{name};  
-        die "Currency unit $unit->{name} is missing a value" unless exists $unit->{value};  
-        die "Currency unit $unit->{name} is not a number" unless looks_like_number($unit->{value});  
-        die "Currency unit $unit->{name} is not positive" unless $unit->{value} > 0; 
+        croak 'Missing currency unit name' unless exists $unit->{name};
+        croak "Currency unit $unit->{name} is missing a value" unless exists $unit->{value};
+        croak "Currency unit $unit->{name} is not a number" unless looks_like_number($unit->{value});
+        croak "Currency unit $unit->{name} is not positive" unless $unit->{value} > 0;
         if ($amount >= $unit->{value}) {
             my $numOfUnits = int($amount/$unit->{value});
             $change->{$unit->{name}} = $numOfUnits;
             $amount -= $numOfUnits * $unit->{value};
         }
-    # if the remaining amount or currency units run out bail out of the loop        
-    } while ($amount > 0 && @{$currency->{units}} > 0);  
+    # if the remaining amount or currency units run out bail out of the loop
+    } while ($amount > 0 && @{$currency->{units}} > 0);
     return $change;
 }
 
-print "Change for 1234.56 with US currency\n" .Data::Dumper->Dumper(change(1234.56))."\n";
+print 'Change for 1234.56 with US currency\n' . Data::Dumper->Dumper(change(1234.56)) . '\n';
 
-my $fiveAndDime = { 
+my $fiveAndDime = {
     base => 100,
     units => [
-        {name => 'Dime',                     value => "10"},  
-        {name => 'Nickel',                   value => 5},  
+        {name => 'Dime',                     value => '10'},
+        {name => 'Nickel',                   value => 5},
     ]
 };
 
-print "Change for 1234.56 at a Five and Dime store (aka only nickels and dimes)\n". 
-    Data::Dumper->Dumper(change(1234.56,$fiveAndDime))."\n";
-    
-    
+print 'Change for 1234.56 at a Five and Dime store (aka only nickels and dimes)\n'.
+    Data::Dumper->Dumper(change(1234.56,$fiveAndDime)).'\n';
+ 
